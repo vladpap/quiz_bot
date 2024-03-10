64520b156c8bef8bde8963380cdb5548d693b0f4
Vladimir <vladimir.papin@gmail.com>
2024-03-10 13:28:42 +0300

    global variables have been removed

--- quiz_tel_bot.py
+++ quiz_tel_bot.py
@@ -1,14 +1,20 @@
 import json
 import logging
 from enum import Enum
+from functools import partial
 from random import choice
 
 import redis
 from environs import Env
 from redis.exceptions import BusyLoadingError, ConnectionError, TimeoutError
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.ext import (ApplicationBuilder, CommandHandler,
-                          ConversationHandler, MessageHandler, filters)
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
 from quiz.quiz import get_random_questions
 
@@ -16,61 +22,67 @@ logger = logging.getLogger(__file__)
 
 
 class ButtonChat(Enum):
-    New_question = 'Новый вопрос ❓'
-    Surrender = 'Сдаться 🤷‍♂️'
-    Score = 'Мой счет 🥇'
+    New_question = "Новый вопрос ❓"
+    Surrender = "Сдаться 🤷‍♂️"
+    Score = "Мой счет 🥇"
 
 
-REPLY_KEYBOARD_NEW_QUESTION = [[
-    ButtonChat.New_question.value, ButtonChat.Score.value]]
+REPLY_KEYBOARD_NEW_QUESTION = [
+    [ButtonChat.New_question.value, ButtonChat.Score.value]
+]
 
-REPLY_KEYBOARD_ANSWER_ATTEMPTS = [[
-    ButtonChat.Score.value, ButtonChat.Surrender.value]]
+REPLY_KEYBOARD_ANSWER_ATTEMPTS = [
+    [ButtonChat.Score.value, ButtonChat.Surrender.value]
+]
 
 
-State = Enum('State', 'NEW_QUESTION ANSWER_ATTEMPTS')
+State = Enum("State", "NEW_QUESTION ANSWER_ATTEMPTS")
 
 
-async def start(update: Update, context):
+async def start(redis_db, update: Update, context):
     chat_id = update.effective_chat.id
     if not redis_db.exists(chat_id):
-        redis_db.set(chat_id, json.dumps({'score': 0}))
+        redis_db.set(chat_id, json.dumps({"score": 0}))
 
     reply_keyboard = REPLY_KEYBOARD_NEW_QUESTION
 
     reply_markup = ReplyKeyboardMarkup(
         reply_keyboard,
         resize_keyboard=True,
-        input_field_placeholder="Ваш выбор ...")
+        input_field_placeholder="Ваш выбор ...",
+    )
 
     await update.effective_message.reply_text(
-            f'Приветствую.\n'
-            f'Нажми "{ButtonChat.New_question.value}" для начала викторины.\n'
-            f'/cancel - для отмены',
-            reply_markup=reply_markup)
+        f"Приветствую.\n"
+        f'Нажми "{ButtonChat.New_question.value}" для начала викторины.\n'
+        f"/cancel - для отмены",
+        reply_markup=reply_markup,
+    )
 
     return State.NEW_QUESTION
 
 
-async def cancel(update: Update, context):
+async def cancel(redis_db, update: Update, context):
     chat_id = update.effective_chat.id
 
     user_base_data = json.loads(redis_db.get(chat_id))
-    if 'task' in user_base_data.keys():
-        del user_base_data['task']
+    if "task" in user_base_data.keys():
+        del user_base_data["task"]
         redis_db.set(chat_id, json.dumps(user_base_data))
 
     reply_markup = ReplyKeyboardRemove()
 
     await update.effective_message.reply_text(
-            'Всего хорошего.\n'
-            'Для начала викторины введите команду /start',
-            reply_markup=reply_markup)
+        "Всего хорошего.\n" "Для начала викторины введите команду /start",
+        reply_markup=reply_markup,
+    )
 
     return ConversationHandler.END
 
 
-async def handle_new_question_request(update: Update, context):
+async def handle_new_question_request(
+    redis_db, random_questions, update: Update, context
+):
     chat_id = update.effective_chat.id
     user_base_data = json.loads(redis_db.get(chat_id))
 
@@ -78,22 +90,25 @@ async def handle_new_question_request(update: Update, context):
     reply_markup = ReplyKeyboardMarkup(
         reply_keyboard,
         resize_keyboard=True,
-        input_field_placeholder="Ваш ответ ...")
+        input_field_placeholder="Ваш ответ ...",
+    )
 
     question_and_answer = choice(random_questions)
 
     await update.effective_message.reply_text(
-            f'Вопрос:\n'
-            f'{question_and_answer["question"]}',
-            reply_markup=reply_markup)
+        f"Вопрос:\n" f'{question_and_answer["question"]}',
+        reply_markup=reply_markup,
+    )
 
-    user_base_data['task'] = question_and_answer
-    user_base_data['task']['count_answer'] = 0
+    user_base_data["task"] = question_and_answer
+    user_base_data["task"]["count_answer"] = 0
     redis_db.set(chat_id, json.dumps(user_base_data))
     return State.ANSWER_ATTEMPTS
 
 
-async def handle_surrender_request(update: Update, context):
+async def handle_surrender_request(
+    redis_db, random_questions, update: Update, context
+):
     chat_id = update.effective_chat.id
     user_base_data = json.loads(redis_db.get(chat_id))
 
@@ -101,117 +116,132 @@ async def handle_surrender_request(update: Update, context):
     reply_markup = ReplyKeyboardMarkup(
         reply_keyboard,
         resize_keyboard=True,
-        input_field_placeholder="Ваш выбор ...")
+        input_field_placeholder="Ваш выбор ...",
+    )
 
     await update.effective_message.reply_text(
-            f'Правильный ответ 🫣:\n'
-            f'{user_base_data["task"]["answer"]}\n\n',
-            reply_markup=reply_markup)
+        f"Правильный ответ 🫣:\n" f'{user_base_data["task"]["answer"]}\n\n',
+        reply_markup=reply_markup,
+    )
 
-    await handle_new_question_request(update=update, context=context)
+    await handle_new_question_request(
+        redis_db, random_questions, update=update, context=context
+    )
 
 
-async def handle_score_request(update: Update, context):
+async def handle_score_request(redis_db, update: Update, context):
     user_base_data = json.loads(redis_db.get(update.effective_chat.id))
     reply = f'Ваш счет:\n{user_base_data["score"]} баллов.'
 
     await update.effective_message.reply_text(reply)
 
 
-async def handle_solution_attempt(update: Update, context):
+async def handle_solution_attempt(redis_db, update: Update, context):
     chat_id = update.effective_chat.id
     user_base_data = json.loads(redis_db.get(chat_id))
     text = update.effective_message.text
     if text.lower() == user_base_data["task"]["answer"]:
         user_base_data["score"] += 1
-        del user_base_data['task']
+        del user_base_data["task"]
         redis_db.set(chat_id, json.dumps(user_base_data))
 
         reply_keyboard = REPLY_KEYBOARD_NEW_QUESTION
         reply_markup = ReplyKeyboardMarkup(
             reply_keyboard,
             resize_keyboard=True,
-            input_field_placeholder="Ваш выбор ...")
+            input_field_placeholder="Ваш выбор ...",
+        )
 
         await update.effective_message.reply_text(
-            f'Правильно! 🥳 Поздравляю!\n'
-            f'Для следующего вопроса нажми «{ButtonChat.New_question.value}»\n'
-            f'/cancel - для отмены',
-            reply_markup=reply_markup)
+            f"Правильно! 🥳 Поздравляю!\n"
+            f"Для следующего вопроса нажми «{ButtonChat.New_question.value}»\n"
+            f"/cancel - для отмены",
+            reply_markup=reply_markup,
+        )
 
         return State.NEW_QUESTION
     else:
-        user_base_data['task']['count_answer'] += 1
+        user_base_data["task"]["count_answer"] += 1
         redis_db.set(chat_id, json.dumps(user_base_data))
-        reply = 'Неправильно… 😪 Попробуешь ещё раз?'
+        reply = "Неправильно… 😪 Попробуешь ещё раз?"
 
-        if user_base_data['task']['count_answer'] > 3:
-            reply += '\nМожно сдаться.'
+        if user_base_data["task"]["count_answer"] > 3:
+            reply += "\nМожно сдаться."
         await update.effective_message.reply_text(reply)
     return State.ANSWER_ATTEMPTS
 
 
-if __name__ == '__main__':
-
+def main():
     logging.basicConfig(
         level=logging.INFO,
-        filename='tel_bot.log', filemode='w',
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        filename="tel_bot.log",
+        filemode="w",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
 
     env = Env()
     env.read_env()
 
-    token_tel_bot = env.str('TELEGRAM_BOT_TOKEN')
-    quiz_files_folder = env.str('QUIZ_FOLDER', default=None)
-    quiz_file_name = env.str('QUIZ_FILE', default=None)
+    token_tel_bot = env.str("TELEGRAM_BOT_TOKEN")
+    quiz_files_folder = env.str("QUIZ_FOLDER", default=None)
+    quiz_file_name = env.str("QUIZ_FILE", default=None)
 
     redis_db = redis.client.Redis(
         decode_responses=True,
-        host=env.str('REDIS_HOST', default='0.0.0.0'),
-        port=env.int('REDIS_PORT', default=6379),
-        socket_timeout=env.int('REDIS_TIMEOUT', default=2),
+        host=env.str("REDIS_HOST", default="0.0.0.0"),
+        port=env.int("REDIS_PORT", default=6379),
+        socket_timeout=env.int("REDIS_TIMEOUT", default=2),
         retry=redis.retry.Retry(
             redis.backoff.ExponentialBackoff(),
-            env.int('REDIS_RETRY', default=3)),
-        retry_on_error=[BusyLoadingError,
-                        ConnectionError,
-                        TimeoutError])
+            env.int("REDIS_RETRY", default=3),
+        ),
+        retry_on_error=[BusyLoadingError, ConnectionError, TimeoutError],
+    )
 
     random_questions = get_random_questions(
-            quiz_folder=quiz_files_folder,
-            quiz_file=quiz_file_name)
+        quiz_folder=quiz_files_folder, quiz_file=quiz_file_name
+    )
     application = ApplicationBuilder().token(token_tel_bot).build()
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-
+        entry_points=[CommandHandler("start", partial(start, redis_db))],
         states={
             State.NEW_QUESTION: [
                 MessageHandler(
                     filters.Regex(ButtonChat.New_question.value),
-                    handle_new_question_request),
+                    partial(
+                        handle_new_question_request, redis_db, random_questions
+                    ),
+                ),
                 MessageHandler(
                     filters.Regex(ButtonChat.Score.value),
-                    handle_score_request),
-                CommandHandler('cancel', cancel)],
-
+                    partial(handle_score_request, redis_db),
+                ),
+                CommandHandler("cancel", partial(cancel, redis_db)),
+            ],
             State.ANSWER_ATTEMPTS: [
                 MessageHandler(
                     filters.Regex(ButtonChat.Surrender.value),
-                    handle_surrender_request),
+                    partial(
+                        handle_surrender_request, redis_db, random_questions
+                    ),
+                ),
                 MessageHandler(
-                    filters.Regex(ButtonChat.Score.value),
-                    handle_score_request),
-                CommandHandler('cancel', cancel),
+                    filters.Regex(ButtonChat.Score.value), handle_score_request
+                ),
+                CommandHandler("cancel", cancel),
                 MessageHandler(
-                    filters.TEXT,
-                    handle_solution_attempt)],
-
+                    filters.TEXT, partial(handle_solution_attempt, redis_db)
+                ),
+            ],
         },
-
-        fallbacks=[CommandHandler('cancel', cancel)]
+        fallbacks=[CommandHandler("cancel", cancel)],
     )
 
     application.add_handler(conv_handler)
 
     application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
--- quiz_vk_bot.py
+++ quiz_vk_bot.py
@@ -17,20 +17,20 @@ logger = logging.getLogger(__file__)
 
 
 class ButtonChat(Enum):
-    New_question = 'Новый вопрос ❓'
-    Surrender = 'Сдаться 🤷‍♂️'
-    Score = 'Мой счет 🥇'
+    New_question = "Новый вопрос ❓"
+    Surrender = "Сдаться 🤷‍♂️"
+    Score = "Мой счет 🥇"
 
 
 def keyboard_new_question():
     keyboard = VkKeyboard(one_time=True)
 
     keyboard.add_button(
-        ButtonChat.New_question.value,
-        color=VkKeyboardColor.SECONDARY)
+        ButtonChat.New_question.value, color=VkKeyboardColor.SECONDARY
+    )
     keyboard.add_button(
-        ButtonChat.Score.value,
-        color=VkKeyboardColor.SECONDARY)
+        ButtonChat.Score.value, color=VkKeyboardColor.SECONDARY
+    )
 
     return keyboard.get_keyboard()
 
@@ -39,45 +39,47 @@ def keyboard_answer_attempts():
     keyboard = VkKeyboard(one_time=True)
 
     keyboard.add_button(
-        ButtonChat.Score.value,
-        color=VkKeyboardColor.SECONDARY)
+        ButtonChat.Score.value, color=VkKeyboardColor.SECONDARY
+    )
     keyboard.add_button(
-        ButtonChat.Surrender.value,
-        color=VkKeyboardColor.SECONDARY)
+        ButtonChat.Surrender.value, color=VkKeyboardColor.SECONDARY
+    )
 
     return keyboard.get_keyboard()
 
 
-def start(vk_api, event):
+def start(redis_db, vk_api, event):
     user_id = event.user_id
     if not redis_db.exists(user_id):
-        redis_db.set(user_id, json.dumps({'score': 0}))
+        redis_db.set(user_id, json.dumps({"score": 0}))
 
     vk_api.messages.send(
         user_id=user_id,
-        message=f'Приветствую.\n'
-                f'Нажми "{ButtonChat.New_question.value}" '
-                f'для начала викторины.\n',
+        message=f"Приветствую.\n"
+        f'Нажми "{ButtonChat.New_question.value}" '
+        f"для начала викторины.\n",
         random_id=get_random_id(),
-        keyboard=keyboard_new_question(),)
+        keyboard=keyboard_new_question(),
+    )
 
 
-def cancel(vk_api, event):
+def cancel(redis_db, vk_api, event):
     user_id = event.user_id
     user_base_data = json.loads(redis_db.get(user_id))
 
-    if 'task' in user_base_data.keys():
-        del user_base_data['task']
+    if "task" in user_base_data.keys():
+        del user_base_data["task"]
         redis_db.set(user_id, json.dumps(user_base_data))
 
     vk_api.messages.send(
         user_id=event.user_id,
-        message='Всего хорошего.\n'
-                'Для начала викторины введите команду start',
-        random_id=get_random_id())
+        message="Всего хорошего.\n"
+        "Для начала викторины введите команду start",
+        random_id=get_random_id(),
+    )
 
 
-def new_question_request(vk_api, event, keyboard):
+def new_question_request(redis_db, random_questions, vk_api, event, keyboard):
     user_id = event.user_id
     user_base_data = json.loads(redis_db.get(user_id))
 
@@ -87,102 +89,110 @@ def new_question_request(vk_api, event, keyboard):
         user_id=user_id,
         message=f'Вопрос:\n{question_and_answer["question"]}',
         random_id=get_random_id(),
-        keyboard=keyboard)
+        keyboard=keyboard,
+    )
 
-    user_base_data['task'] = question_and_answer
-    user_base_data['task']['count_answer'] = 0
+    user_base_data["task"] = question_and_answer
+    user_base_data["task"]["count_answer"] = 0
     redis_db.set(user_id, json.dumps(user_base_data))
     return
 
 
-def solution_attempt(vk_api, event):
+def solution_attempt(redis_db, vk_api, event):
     user_id = event.user_id
     user_base_data = json.loads(redis_db.get(user_id))
     text = event.text
     if text.lower() == user_base_data["task"]["answer"]:
         user_base_data["score"] += 1
-        del user_base_data['task']
+        del user_base_data["task"]
         redis_db.set(user_id, json.dumps(user_base_data))
 
         vk_api.messages.send(
             user_id=user_id,
-            message=f'Правильно! 🥳 Поздравляю!\n'
-                    f'Для следующего вопроса нажми'
-                    f' «{ButtonChat.New_question.value}»\n'
-                    f'cancel - для отмены',
+            message=f"Правильно! 🥳 Поздравляю!\n"
+            f"Для следующего вопроса нажми"
+            f" «{ButtonChat.New_question.value}»\n"
+            f"cancel - для отмены",
             random_id=get_random_id(),
-            keyboard=keyboard_new_question())
+            keyboard=keyboard_new_question(),
+        )
 
         return True
     else:
-        user_base_data['task']['count_answer'] += 1
+        user_base_data["task"]["count_answer"] += 1
         redis_db.set(user_id, json.dumps(user_base_data))
-        reply = 'Неправильно… 😪 Попробуешь ещё раз?'
+        reply = "Неправильно… 😪 Попробуешь ещё раз?"
 
-        if user_base_data['task']['count_answer'] > 3:
-            reply += '\nМожно сдаться.'
+        if user_base_data["task"]["count_answer"] > 3:
+            reply += "\nМожно сдаться."
 
         vk_api.messages.send(
             user_id=user_id,
             message=reply,
             random_id=get_random_id(),
-            keyboard=keyboard_answer_attempts())
+            keyboard=keyboard_answer_attempts(),
+        )
     return False
 
 
-def handle_surrender_request(vk_api, event):
+def handle_surrender_request(redis_db, random_questions, vk_api, event):
     user_id = event.user_id
     user_from_base = json.loads(redis_db.get(user_id))
 
     vk_api.messages.send(
         user_id=user_id,
-        message=f'Правильный ответ 🫣:\n'
-                f'{user_from_base["task"]["answer"]}\n\n',
-        random_id=get_random_id())
+        message=f"Правильный ответ 🫣:\n"
+        f'{user_from_base["task"]["answer"]}\n\n',
+        random_id=get_random_id(),
+    )
 
     new_question_request(
-        vk_api, event, keyboard_answer_attempts())
+        redis_db, random_questions, vk_api, event, keyboard_answer_attempts()
+    )
 
 
-def handle_score_request(vk_api, event, keyboard):
+def handle_score_request(redis_db, vk_api, event, keyboard):
     user_id = event.user_id
     user_base_data = json.loads(redis_db.get(user_id))
     vk_api.messages.send(
-            user_id=user_id,
-            message=f'Ваш счет:\n{user_base_data["score"]} баллов.',
-            random_id=get_random_id(),
-            keyboard=keyboard)
+        user_id=user_id,
+        message=f'Ваш счет:\n{user_base_data["score"]} баллов.',
+        random_id=get_random_id(),
+        keyboard=keyboard,
+    )
 
 
-if __name__ == "__main__":
+def main():
     logging.basicConfig(
         level=logging.INFO,
-        filename='vk_bot.log', filemode='w',
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        filename="vk_bot.log",
+        filemode="w",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
 
     env = Env()
     env.read_env()
 
-    quiz_files_folder = env.str('QUIZ_FOLDER', default=None)
-    quiz_file_name = env.str('QUIZ_FILE', default=None)
+    quiz_files_folder = env.str("QUIZ_FOLDER", default=None)
+    quiz_file_name = env.str("QUIZ_FILE", default=None)
 
     redis_db = redis.client.Redis(
         decode_responses=True,
-        host=env.str('REDIS_HOST', default='0.0.0.0'),
-        port=env.int('REDIS_PORT', default=6379),
-        socket_timeout=env.int('REDIS_TIMEOUT', default=2),
+        host=env.str("REDIS_HOST", default="0.0.0.0"),
+        port=env.int("REDIS_PORT", default=6379),
+        socket_timeout=env.int("REDIS_TIMEOUT", default=2),
         retry=redis.retry.Retry(
             redis.backoff.ExponentialBackoff(),
-            env.int('REDIS_RETRY', default=3)),
-        retry_on_error=[BusyLoadingError,
-                        ConnectionError,
-                        TimeoutError])
+            env.int("REDIS_RETRY", default=3),
+        ),
+        retry_on_error=[BusyLoadingError, ConnectionError, TimeoutError],
+    )
 
     random_questions = get_random_questions(
-            quiz_folder=quiz_files_folder,
-            quiz_file=quiz_file_name)
+        quiz_folder=quiz_files_folder, quiz_file=quiz_file_name
+    )
 
-    token_vk_bot = env.str('VK_COMMUNITY_TOKEN')
+    token_vk_bot = env.str("VK_COMMUNITY_TOKEN")
     vk_session = vk.VkApi(token=token_vk_bot)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
@@ -194,43 +204,58 @@ if __name__ == "__main__":
             if is_new_question:
                 match event.text:
 
-                    case 'start':
-                        start(vk_api, event)
+                    case "start":
+                        start(redis_db, vk_api, event)
 
-                    case 'cancel':
+                    case "cancel":
                         is_new_question = True
-                        cancel(vk_api, event)
+                        cancel(redis_db, vk_api, event)
 
                     case ButtonChat.New_question.value:
                         is_new_question = False
                         new_question_request(
-                            vk_api, event, keyboard_answer_attempts())
+                            redis_db,
+                            random_questions,
+                            vk_api,
+                            event,
+                            keyboard_answer_attempts(),
+                        )
 
                     case ButtonChat.Score.value:
                         handle_score_request(
-                            vk_api, event, keyboard_new_question())
+                            redis_db, vk_api, event, keyboard_new_question()
+                        )
 
                     case _:
                         vk_api.messages.send(
                             user_id=event.user_id,
                             message=f'Нажми "{ButtonChat.New_question.value}"'
-                                    f' для начала викторины.\n',
+                            f" для начала викторины.\n",
                             random_id=get_random_id(),
-                            keyboard=keyboard_new_question())
+                            keyboard=keyboard_new_question(),
+                        )
             else:
                 match event.text:
 
-                    case 'cancel':
+                    case "cancel":
                         is_new_question = True
                         cancel(vk_api, event)
 
                     case ButtonChat.Surrender.value:
-                        handle_surrender_request(vk_api, event)
+                        handle_surrender_request(
+                            redis_db, random_questions, vk_api, event
+                        )
 
                     case ButtonChat.Score.value:
                         handle_score_request(
-                            vk_api, event, keyboard_answer_attempts())
+                            vk_api, event, keyboard_answer_attempts()
+                        )
 
                     case _:
                         is_new_question = solution_attempt(
-                            vk_api, event)
+                            redis_db, vk_api, event
+                        )
+
+
+if __name__ == "__main__":
+    main()
